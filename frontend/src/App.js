c3b897b85c9d0259a6c54fd9257a8a8fbee5e55f
KL Sandaruwan <lahirusandaruwan501@gmail.com>
2023-09-04 15:20:01 +0530

    add Student

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import CounterClass from "./components/CounterClass";
 import CounterFunction from "./components/CounterFunction";
+import Header from "./components/Header";
+import AddBook from "./components/addBook";
+import { BrowserRouter as Router, Route ,Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <h1> Hello React </h1>
-     <CounterClass />
-     <CounterFunction />
-
-    </div>
+    <Router>
+      <div>
+        <Header/>
+        <Routes>
+        <Route path ="/add" element={<AddBook/>} />
+        </Routes>
+        
+     </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/addBook.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+
+function AddBook() {
+  // Define state variables to store form input values
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publicationYear, setPublicationYear] = useState('');
+  const [ISBN, setISBN] = useState('');
+  const [genre, setGenre] = useState('');
+
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Create a book object with the form data
+    const newBook = {
+      title,
+      author,
+      publicationYear,
+      ISBN,
+      genre,
+    };
+
+    console.log(newBook);
+
+    // Send the new book data to your backend for saving
+    // You can use a fetch or Axios to make a POST request to your backend API
+
+    // Reset the form fields
+    setTitle('');
+    setAuthor('');
+    setPublicationYear('');
+    setISBN('');
+    setGenre('');
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+    <div>
+      <h2>Add a New Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="author" className="form-label">
+            Author
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="publicationYear" className="form-label">
+            Publication Year
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="publicationYear"
+            value={publicationYear}
+            onChange={(e) => setPublicationYear(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="ISBN" className="form-label">
+            ISBN
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="ISBN"
+            value={ISBN}
+            onChange={(e) => setISBN(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="genre" className="form-label">
+            Genre
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="genre"
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+    </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddBook;
